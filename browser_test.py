1f7aa6743946e9d6eac26f02e474efb5fab2c330
Vladimir Zindovic <vladimirzind@gmail.com>
2017-09-29 13:59:28 +0200

    adding chrome
    initializing chrome driver

--- browser_test.py
+++ browser_test.py
@@ -14,3 +14,10 @@ from selenium.webdriver.opera.webdriver import WebDriver
 from selenium.webdriver.ie.webdriver import WebDriver as IE
 from time import time
 from time import sleep
+
+# for chrome
+# set the path to chromedriver AND 
+# remove the annoying infobar telling me that Chrome is being controlled by automated software
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--disable-infobars")
+driver = webdriver.Chrome("C:\\folded\\browsdrv\\krom\\chromedriver.exe", chrome_options=chrome_options)
