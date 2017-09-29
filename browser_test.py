684a8e1cb22afa7551d696ba0b5121fe808b8739
Vladimir Zindovic <vladimirzind@gmail.com>
2017-09-29 14:48:51 +0200

    back to print

--- browser_test.py
+++ browser_test.py
@@ -63,6 +63,6 @@ def choosebrowser(browser):
         driver = webdriver.Ie(executable_path="C:\\folded\\browsdrv\\ied\\IEDriverServer32.exe", capabilities=capabilities)
         return driver
     else:
-        return("Wrong value. Please enter one of the following: chrome, firefox, opera, edge, ie")
+        print("Wrong value. Please enter one of the following: chrome, firefox, opera, edge, ie")
 
     
